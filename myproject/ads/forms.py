ba59f7be0d4e5880ac4767710b5ec93ca90308ec
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2025-01-15 16:57:34 +0300

    Update forms.py

--- myproject/ads/forms.py
+++ myproject/ads/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Ad
+from django.contrib.auth.models import User
 
 class AdForm(forms.ModelForm):
+    email = forms.EmailField(required=True)  # Обязательное поле для электронной почты
+
     class Meta:
-        model = Ad
-        fields = ['title', 'description']
+        model = User
+        fields = ['username', 'email', 'password']  # Добавляем email в поля формы
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])  # Устанавливаем пароль
+        if commit:
+            user.save()
+        return user
