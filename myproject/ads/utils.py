93261e8f74de3c2ace1dc85889d83adaccaa2498
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2025-01-15 16:58:20 +0300

    Update utils.py

--- myproject/ads/utils.py
+++ myproject/ads/utils.py
@@ -1,6 +1,34 @@
-import random
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import OneTimeCode
+import random
 
 def send_one_time_code(user):
-    code = str(random.randint(100000, 999999))  # Генерация кода
-    OneTimeCode.objects.create(user=user, code=code)
+    """
+    Отправляет одноразовый код на электронную почту пользователя.
+    """
+    code = str(random.randint(100000, 999999))  # Генерация одноразового кода
+    subject = 'Ваш одноразовый код'
+    message = f'Ваш одноразовый код: {code}'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [user.email]
+
+    # Отправка письма
+    send_mail(subject, message, email_from, recipient_list)
+
+    # Сохранение кода в базе данных
+    one_time_code, created = OneTimeCode.objects.get_or_create(user=user)
+    one_time_code.code = code
+    one_time_code.save()
+
+def notify_ad_creation(ad):
+    """
+    Отправляет уведомление автору объявления о создании объявления.
+    """
+    subject = 'Ваше объявление создано'
+    message = f'Ваше объявление "{ad.title}" было успешно создано.'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [ad.user.email]
+
+    # Отправка письма
+    send_mail(subject, message, email_from, recipient_list)
